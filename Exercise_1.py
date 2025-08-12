acc4877bf0793fc6d27b91272cf9f7e67f10d78e
unknown <kalicheti.sathvika@gmail.com>
2025-08-12 16:36:47 -0500

    pre couse 1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,52 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+    def __init__(self):
+      self.arr=[]
+      self.size = 0
+    def isEmpty(self):
+      """Time complexity : O(1)
+      """
+      self.size == 0
+       
          
-     def isEmpty(self):
+    def push(self, item):
+      """Time complexity : O(1)
+      """
+      self.arr.append(item)
+      self.size+=1
          
-     def push(self, item):
-         
-     def pop(self):
+    def pop(self):
+      """Time complexity : O(1)
+      """
+      if self.isEmpty():
+        return None
+      self.size-=1
+      return self.arr.pop()
         
+    def peek(self):
+      """Time complexity : O(1)
+      """
+      if self.isEmpty():
+        return None
+      return self.arr[-1]
+      
         
-     def peek(self):
-        
-     def size(self):
+    def size(self):
+      """Time complexity : O(1)
+      """
+      return self.size
          
-     def show(self):
-         
+    def show(self):
+      """Time complexity : O(n)
+      """
+      return self.arr[::-1]
+      """Space Complexity: O(n) - for storing the n elements in the stack 
+      """
 
 s = myStack()
 s.push('1')
 s.push('2')
+s.push('3')
 print(s.pop())
 print(s.show())
